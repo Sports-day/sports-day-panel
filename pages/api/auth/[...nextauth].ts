b9af3c6edafd8a9636f06875c134c09798fd0831
testusuke <silverbro007@icloud.com>
2023-02-17 11:24:38 +0900

    feat: azure ad

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth"
+import AzureADProvider from "next-auth/providers/azure-ad"
+export const authOptions = {
+    providers: [
+        AzureADProvider({
+            // @ts-ignore
+            clientId: process.env.AZUREAD_CLIENT_ID,
+            // @ts-ignore
+            clientSecret: process.env.AZUREAD_CLIENT_SECRET,
+            tenantId: process.env.AZUREAD_TENANT_ID,
+        }),
+    ],
+    secret: process.env.SECRET,
+    callbacks: {
+        //  @ts-ignore
+        async signIn({ user, account, profile, email, credentials }) {
+            console.log("account", account)
+
+            //  Request access to backend
+
+            return true
+        },
+    },
+}
+
+// @ts-ignore
+export default NextAuth(authOptions)
