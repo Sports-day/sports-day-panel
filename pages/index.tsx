a4aa843df7bd4965f3d75865d1354a9d1396f92d
1nayu <nayu0386@gmail.com>
2023-04-09 00:12:56 +0900

    Commit01:add basic appearance

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,12 @@
 import type {NextPage} from 'next'
 import {signIn, signOut, useSession} from "next-auth/react";
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
+import Image from 'next/image'
+import styles from 'styles/Auth.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
 
 const Home: NextPage = () => {
     const {data: session} = useSession()
@@ -17,8 +24,45 @@ const Home: NextPage = () => {
         //  ログイン待ち
         return (
             <>
-                Not signed in <br/>
-                <button onClick={() => signIn("azure-ad", {callbackUrl: "/"})}>Sign in</button>
+                <Head>
+                    <title>SPORTSDAY : Login</title>
+                    <meta name="description" content="SPORTSDAY Login page" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+                <Head>
+                    <title>SPORTSDAY : Login</title>
+                    <meta name="description" content="SPORTSDAY Login page" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+                <main className={styles.main}>
+                    <div className={styles.catch}>
+                        <h1 className={inter.className}>みんなの</h1>
+                        <h1 className={inter.className}>球技大会、</h1>
+                        <h1 className={inter.className}>あなたの</h1>
+                        <Image
+                            src="/logo.svg"
+                            alt="SPORTSDAY Logo"
+                            className={styles.authLogo}
+                            width={304}
+                            height={36}
+                            priority
+                        />
+                    </div>
+                    <div className={styles.authButton}>
+                        <a className={inter.className} onClick={() => signIn("azure-ad", {callbackUrl: "/"})}>
+                            <Image
+                                src="/ms.svg"
+                                alt="Microsoft Logo"
+                                className={styles.msLogo}
+                                width={20}
+                                height={20}
+                                priority
+                            />
+                            KOSENアカウントではじめよう</a>
+                    </div>
+                </main>
             </>
         )
     }
