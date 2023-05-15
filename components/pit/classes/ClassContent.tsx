183fc4d4c65b118170cbdcc30b09f2f9c836f91c
testusuke <silverbro007@icloud.com>
2023-05-15 22:23:06 +0900

    feat: classes

---
+++ components/pit/classes/ClassContent.tsx
@@ -0,0 +1,72 @@
+import {Button, TableCell, TableRow} from "@mui/material";
+import {useState} from "react";
+import {ConfirmDialog} from "../ConfirmDialog";
+import {Class, classFactory} from "../../../src/models/ClassModel";
+import {useFetchGroup} from "../../../src/features/groups/hook";
+import {ClassForm} from "./ClassForm";
+
+export function ClassContent(props: { class: Class, refresh: VoidFunction }) {
+    const [isEditOpen, setIsEditOpen] = useState(false)
+    const [isDeleteOpen, setIsDeleteOpen] = useState(false)
+    const {group} = useFetchGroup(props.class.groupId)
+
+    const deleteClass = async () => {
+        await classFactory().delete(props.class.id)
+        props.refresh()
+        setIsDeleteOpen(false)
+    }
+
+    return (
+        <>
+            <TableRow
+                sx={{'&:last-child td, &:last-child th': {border: 0}}}
+            >
+                <TableCell>{props.class.id}</TableCell>
+                <TableCell>{props.class.name}</TableCell>
+                <TableCell>{props.class.description}</TableCell>
+                <TableCell>{group?.name}</TableCell>
+                <TableCell
+                    sx={{
+                        width: "200px"
+                    }}
+                >
+                    {new Date(props.class.updatedAt).toLocaleString("ja-JP")}
+                </TableCell>
+                <TableCell
+                    sx={{
+                        width: "200px"
+                    }}
+                >
+                    <Button
+                        onClick={() => setIsEditOpen(true)}
+                    >
+                        編集
+                    </Button>
+                    <Button
+                        onClick={() => setIsDeleteOpen(true)}
+                    >
+                        削除
+                    </Button>
+                </TableCell>
+            </TableRow>
+            {/*edit*/}
+            <ClassForm
+                isOpen={isEditOpen}
+                setClose={() => setIsEditOpen(false)}
+                formType={"edit"}
+                refresh={props.refresh}
+                class={props.class}
+            />
+            {/*delete*/}
+            <ConfirmDialog
+                open={isDeleteOpen}
+                onClose={() => setIsDeleteOpen(false)}
+                onConfirm={() => deleteClass()}
+                confirmText={"削除"}
+                confirmColor={"warning"}
+            >
+                <p>{props.class.name}を削除しますか？</p>
+            </ConfirmDialog>
+        </>
+    )
+}
---
+++ components/pit/classes/Classes.tsx
@@ -0,0 +1,49 @@
+import {useFetchClasses} from "../../../src/features/classes/hooks";
+import {useState} from "react";
+import styles from "../../../styles/Pit.module.scss";
+import {Button} from "@mui/material";
+import {ClassesContext, GroupsContext} from "../context";
+import {ClassList} from "./ClassList";
+import {useFetchGroups} from "../../../src/features/groups/hook";
+import {ClassForm} from "./ClassForm";
+
+export function Classes() {
+    const {classes, refresh: refreshClasses} = useFetchClasses()
+    const {groups, refresh: refreshGroups} = useFetchGroups()
+    const [isCreatorOpen, setIsCreatorOpen] = useState(false)
+
+
+    return (
+        <>
+            <ClassesContext.Provider value={{data: classes, refresh: refreshClasses}}>
+                <GroupsContext.Provider value={{data: groups, refresh: refreshGroups}}>
+                <div className={styles.content}>
+
+                    <h1>クラス</h1>
+                    <p>クラスを使ってチームやユーザーを管理できます。</p>
+
+                    <Button
+                        variant="contained"
+                        sx={{
+                            position: "absolute",
+                            right: "20px",
+                            top: "20px",
+                        }}
+                        onClick={() => setIsCreatorOpen(true)}
+                    >
+                        作成
+                    </Button>
+
+                    <ClassForm
+                        isOpen={isCreatorOpen}
+                        setClose={() => setIsCreatorOpen(false)}
+                        formType={"create"}
+                        refresh={refreshClasses}
+                    />
+                    <ClassList/>
+                </div>
+                </GroupsContext.Provider>
+            </ClassesContext.Provider>
+        </>
+    )
+}
