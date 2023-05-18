02619329f6bf9b504c9707020bb8037291efffd6
testusuke <silverbro007@icloud.com>
2023-05-18 09:24:51 +0900

    feat: Dashboard head

--- components/pit/PitDashboard.tsx
+++ components/pit/PitDashboard.tsx
@@ -2,10 +2,15 @@ import {PitHeader} from "./PitHeader";
 import {PitSidebar} from "./PitSidebar";
 import styles from "../../styles/Pit.module.scss";
 import {ReactNode} from "react";
+import Head from "next/head";
 
 export function PitDashboard(props: {children: ReactNode}) {
     return (
         <>
+            <Head>
+                <title>Pit</title>
+                <meta name="description" content="Pit Dashboard"/>
+            </Head>
             <div className={styles.pit}>
                 <PitHeader/>
                 <div className={styles.dashboard}>
