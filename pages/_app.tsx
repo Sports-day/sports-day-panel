384224ba0dc033d3c2d328a28b4cef42059da4bb
testusuke <silverbro007@icloud.com>
2023-05-18 09:24:41 +0900

    change: import global css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type {AppProps} from 'next/app'
 import {SessionProvider} from "next-auth/react"
 import {Session} from "next-auth";
+import "../styles/globals.css";
 
 export default function MyApp({
                                   Component,
