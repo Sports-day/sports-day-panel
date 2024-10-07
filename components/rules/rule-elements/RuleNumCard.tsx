797ae832e7e6d68ee3d95d577003ad048599e0c4
tosnayu <y.nakamura@tos-system.co.jp>
2024-10-07 22:16:09 +0900

    fix: fixed layout collapse in rule

--- components/rules/rule-elements/RuleNumCard.tsx
+++ components/rules/rule-elements/RuleNumCard.tsx
@@ -1,48 +1,33 @@
-import {Stack, Typography, useTheme}from "@mui/material";
+import {Divider, Stack, Typography, useTheme} from "@mui/material";
 
 export type RuleNumCardProps = {
     title: string;
     content: string;
-    sub: string;
+    sub?: string;
 }
 
 export const RuleNumCard = (props: RuleNumCardProps) => {
     const theme = useTheme();
     const {title, content, sub} = props;
     return (
-        <Stack
-            direction={"column"}
-            justifyContent={"space-between"}
-            alignItems={"flex-start"}
-            spacing={1}
-            px={1.5}
-            py={1.5}
-            mr={1}
-            mb={1}
-            sx={{
-                border: `1px solid ${theme.palette.text.secondary}`,
-                borderRadius: "15px",
-                borderBottomLeftRadius: "3px",
-                height:"fit-content",
-            }}
-        >
-            <Typography
-                color={theme.palette.text.secondary}
-                sx={{fontSize: "16px"}}
-            >
-                {title}
-            </Typography>
-            <Typography
-                color={theme.palette.text.primary}
-                fontSize={"24px"}
-            >
-                {content}
-            </Typography>
-            <Typography
-                color={theme.palette.text.secondary}
-            >
-                {sub}
-            </Typography>
+        <Stack sx={{py:2, px:3, my:1, width:"100%",
+            background: `${theme.palette.text.disabled}20`,
+            boxShadow: `0px 2px 15px ${theme.palette.text.disabled}25`,
+            border:`1px solid ${theme.palette.text.disabled}20`,
+            borderRadius:"16px"
+        }}>
+            <Stack spacing={0.5}>
+                <Stack direction={"row"} spacing={1} width={"100%"} alignItems={"center"} justifyContent={"space-between"}>
+                    <Typography fontSize={"14px"}>{title}</Typography>
+                    <Typography fontWeight={"600"}>{content}</Typography>
+                </Stack>
+                {sub &&
+                    <>
+                        <Divider/>
+                        <Typography fontSize={"14px"} pt={1}>{sub}</Typography>
+                    </>
+                }
+            </Stack>
         </Stack>
 
     )
