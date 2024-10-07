6ebc9f2cec1123d1f8cc1892da6c5e36047558e7
tosnayu <y.nakamura@tos-system.co.jp>
2024-10-07 22:16:36 +0900

    fix: add rules for imizu 2024 fall

--- components/rules/DocsOverall.tsx
+++ components/rules/DocsOverall.tsx
@@ -22,26 +22,28 @@ import ReactMarkdown from "react-markdown";
 import * as React from "react";
 
 const markdown = `
-# 運営体制
-- 運営： ${process.env.NEXT_PUBLIC_OPERATION_PERSON}
-- 結果集計：${process.env.NEXT_PUBLIC_RESULT_PERSON}
-- 巡回・状況把握： ${process.env.NEXT_PUBLIC_PATROL_PERSON}
-- 取材： ${process.env.NEXT_PUBLIC_INTERVIEW_PERSON}
-- 緊急対応： ${process.env.NEXT_PUBLIC_EMERGENCY_PERSON}
+# 会場
+- ビーチバレー：第1体育館
+- バスケットボール：第2体育館
+- ドッジボール：野球場
+- サッカー：グラウンド 芝生一面
 
 # ⚠️注意事項
-安全で楽しい大会になるよう、下記のことに気をつけましょう。
 
-◇熱中症は命の危険があり，治ったとしても後遺症が残ることがある非常に危険性の高いものです。したがって競技等よりも熱中症対策を最優先してください。
-- 立ちくらみ，めまい，筋肉痛，手や足がつる，汗を大量にかく，生あくびが出るなどは熱中症の初期症状が疑われます。さらに症状が進むと頭痛，倦怠感，虚脱感，判断力や集中力の低下などが起こります。少しでも体調の異変を感じたら，試合途中でもただちに活動を終え２ｍ以上距離を取り、マスクを外し、声を出さずに休養してください。
-- また、近くの教職員に体調不良を伝えてください。自分だけでなく，周りの人も様子に変化がないか注意してください。
-- 喉が渇いていなくてもこまめに水分および塩分を補給し，休息もこまめにとってください
+- 楽しむこと。
+- 各自準備運動をしておき、けがの予防に努めること。
+- 試合開始 10 分前には各競技場所に集合すること。
+- 開始時刻までにメンバーが揃わなかった場合、その場にいるメンバーで始め、遅れてきた人は途中参加とする。
+- 体育館への土足での進入および体育館での飲食は厳禁とする。
+- 親睦を深めることが重要である。怪我人を出すほど競技に熱中しないこと。
+- エントリー外の選手の出場は禁止とする。
+- 審判の指示に従うこと。
 
-◇新型コロナウイルス感染防止対策のため、以下の注意事項を遵守してください
-- 競技中のマスク着用は競技者の判断とする。
-- 試合前後の手洗い、消毒を徹底すること。
-- 観戦時や屋内で会話をする際、人との距離を十分に保てない場合はマスクを着用すること。
-- 更衣室内が密にならないよう、混雑を避けるようにすること。
+# 営業時間
+- 生協売店（ポプリ）：09時30分　〜　17時00分
+- 生協食堂：11時30分　〜　13時30分
+- 学寮食堂：11時30分　〜　13時30分
+
 `
 
 export const DocsOverall = () => {
@@ -64,14 +66,14 @@ export const DocsOverall = () => {
             }}>
                 <TimelineItem>
                     <TimelineOppositeContent color={theme.palette.text.secondary}>
-                        08:50
+                        09:05
                     </TimelineOppositeContent>
                     <TimelineSeparator>
                         <TimelineDot/>
                         <TimelineConnector/>
                     </TimelineSeparator>
                     <TimelineContent>
-                        開会式
+                        出欠確認
                         <Stack direction={"row"}>
                             <Box pt={0.2}><HiMapPin size={16} color={theme.palette.text.secondary}/></Box>
                             <Typography fontSize={"14px"} color={theme.palette.text.secondary}>各教室</Typography>
@@ -80,18 +82,18 @@ export const DocsOverall = () => {
                 </TimelineItem>
                 <TimelineItem>
                     <TimelineOppositeContent color={theme.palette.text.secondary}>
-                        09:00
+                        09:15
                     </TimelineOppositeContent>
                     <TimelineSeparator>
                         <TimelineDot/>
                         <TimelineConnector/>
                     </TimelineSeparator>
                     <TimelineContent>
-                        着替え
+                        開会式（放送）
                         <Stack direction={"row"}>
                             <Box pt={0.2}><HiMapPin size={16} color={theme.palette.text.secondary}/></Box>
                             <Typography fontSize={"14px"} color={theme.palette.text.secondary}>
-                                男子は各教室、女子は女子更衣室と1階物理実験室
+                                各教室
                             </Typography>
                         </Stack>
                     </TimelineContent>
@@ -100,7 +102,7 @@ export const DocsOverall = () => {
                     <TimelineOppositeContent color={theme.palette.text.secondary}>
                         09:30
                         〜
-                        15:20
+                        15:40
                     </TimelineOppositeContent>
                     <TimelineSeparator>
                         <TimelineDot/>
@@ -110,7 +112,23 @@ export const DocsOverall = () => {
                 </TimelineItem>
                 <TimelineItem>
                     <TimelineOppositeContent color={theme.palette.text.secondary}>
-                        15:30
+                        15:50
+                    </TimelineOppositeContent>
+                    <TimelineSeparator>
+                        <TimelineDot/>
+                        <TimelineConnector/>
+                    </TimelineSeparator>
+                    <TimelineContent>
+                        出欠確認
+                        <Stack direction={"row"}>
+                            <Box pt={0.2}><HiMapPin size={16} color={theme.palette.text.secondary}/></Box>
+                            <Typography fontSize={"14px"} color={theme.palette.text.secondary}>各教室</Typography>
+                        </Stack>
+                    </TimelineContent>
+                </TimelineItem>
+                <TimelineItem>
+                    <TimelineOppositeContent color={theme.palette.text.secondary}>
+                        15:55
                     </TimelineOppositeContent>
                     <TimelineSeparator>
                         <TimelineDot/>
@@ -126,7 +144,7 @@ export const DocsOverall = () => {
                 </TimelineItem>
                 <TimelineItem>
                     <TimelineOppositeContent color={theme.palette.text.secondary}>
-                        15:50
+                        16:10
                     </TimelineOppositeContent>
                     <TimelineSeparator>
                         <TimelineDot/>
