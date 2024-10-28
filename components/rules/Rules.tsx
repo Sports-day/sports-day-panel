c90dc499c36dd74e8140820a0e2ba535754f70fd
1nayu <nayu0386@gmail.com>
2024-10-28 23:35:34 +0900

    change: rules for beachball, dodgebee

--- components/rules/Rules.tsx
+++ components/rules/Rules.tsx
@@ -3,6 +3,11 @@ import {RuleBeachball} from "./RuleBeachball";
 import {RuleBasketball} from "./RuleBasketball"
 import {RuleDodgeBall} from "@/components/rules/RuleDodgeBall";
 import {RuleSoccer} from "@/components/rules/RuleSoccer";
+import {RuleDodgebee} from "@/components/rules/RuleDodgebee";
+import {RuleBadminton} from "@/components/rules/RuleBadminton";
+import {RuleFutsal} from "@/components/rules/RuleFutsal";
+import {RuleStrackout} from "@/components/rules/RuleStrackout";
+import {RulePetanque} from "@/components/rules/RulePetanque";
 
 export type RuleProps = {
     ruleId: number;
@@ -10,9 +15,12 @@ export type RuleProps = {
 
 export const Rules = (props: RuleProps) => {
     const {ruleId} = props;
-    if (ruleId === 1){return <RuleBasketball/>}
-    if (ruleId === 2){return <RuleDodgeBall/>}
-    if (ruleId === 3){return <RuleBeachball/>}
-    if (ruleId === 4){return <RuleSoccer/>}
+    if (ruleId === 1){return <RuleBeachball/>}
+    if (ruleId === 2){return <RuleDodgebee/>}
+    if (ruleId === 3){return <RuleBadminton/>}
+    if (ruleId === 4){return <RuleFutsal/>}
+    if (ruleId === 5){return <RuleStrackout/>}
+    if (ruleId === 6){return <RulePetanque/>}
+
     return(<> </>)
 }
