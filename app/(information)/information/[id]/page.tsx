c14aa3a98edb9f38bec14651df4e138b5853aecb
Takkun0310 <takkunsan0310@gmail.com>
2024-10-30 02:17:17 +0900

    change: 各要素のレイヤー調整

--- app/(information)/information/[id]/page.tsx
+++ app/(information)/information/[id]/page.tsx
@@ -1,24 +1,50 @@
 'use client'
 import {Box, Container} from "@mui/material";
+import LeftCircleContainer from "@/components/layouts/leftCircleContainer";
 
 export default function Page({params}: { params: { id: string } }) {
 
     return (
-        <Container maxWidth="lg">
-            <Box display="flex" flexDirection="row" justifyContent="center" alignItems="center" gap={4}>
-                {/* First Component */}
-                <Box flex={1}>
-                    {/* コンポーネント1の内容をここに追加 */}
-                    <Box>Component 1</Box>
-                </Box>
-
-                {/* Second Component */}
-                <Box flex={1}>
-                    {/* コンポーネント2の内容をここに追加 */}
-                    <Box>Component 2</Box>
-                </Box>
+        <div style={{position: 'relative'}}>
+            <Box
+                sx={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
+                    zIndex: 0
+                }}
+            >
+                <LeftCircleContainer/>
             </Box>
-        </Container>
 
+
+            <Container
+                maxWidth="lg"
+                sx={{
+                    position: 'relative',
+                    zIndex: 1
+                }}
+            >
+                <Box
+                    display="flex"
+                    flexDirection="row"
+                    justifyContent="center"
+                    alignItems="center"
+                    gap={4}
+                >
+                    {/* First Component */}
+                    <Box flex={1}>
+                        <Box>Component 1</Box>
+                    </Box>
+
+                    {/* Second Component */}
+                    <Box flex={1}>
+                        <Box>Component 2</Box>
+                    </Box>
+                </Box>
+            </Container>
+        </div>
     );
 }
