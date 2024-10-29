77e3d308b38009f269ade1c844c878ec5ab54894
testusuke <testusuke@gmail.com>
2024-10-29 10:50:26 +0900

    fix: page.tsxに雛形を追加

---
+++ app/(information)/information/page.tsx
@@ -0,0 +1,10 @@
+import {Container, Typography} from "@mui/material";
+
+export default function Page() {
+    return (
+        <Container maxWidth="lg">
+            <Typography variant="h1">競技一覧を追加</Typography>
+        </Container>
+
+    )
+}
