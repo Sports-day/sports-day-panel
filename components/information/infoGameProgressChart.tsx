98f6a33d0f651c06917bc0036cc2d1c470d8de4a
Takkun0310 <takkunsan0310@gmail.com>
2024-10-31 18:09:40 +0900

    feat: info用ProgressChartの追加

---
+++ components/information/infoGameProgressChart.tsx
@@ -0,0 +1,55 @@
+'use client'
+import {Box, LinearProgress, linearProgressClasses, Stack, styled, Typography, useTheme} from "@mui/material";
+import * as React from "react";
+import {useContext} from "react";
+import {TeamsContext} from "../context";
+
+type Props = {
+    chartSeries: number[]
+}
+
+export const InfoGameProgressChart = (props: Props) => {
+    const theme = useTheme();
+    const {data: teams} = useContext(TeamsContext);
+    const PointBar = styled(LinearProgress)(({}) => ({
+        height: 7,
+        borderRadius: 3,
+        [`&.${linearProgressClasses.colorPrimary}`]: {backgroundColor: theme.palette.secondary.dark,},
+        [`& .${linearProgressClasses.bar}`]: {borderRadius: 2, backgroundColor: theme.palette.text.primary,},
+    }));
+
+    return (
+        <>
+
+            <Stack
+                direction={"column"}
+                justifyContent={"space-between"}
+                alignItems={"space-between"}
+                maxWidth={'xl'}
+                sx={{width: '100%', height: "100%"}}
+                spacing={1}
+            >
+                <Stack
+                    direction={"row"}
+                    justifyContent={"space-between"}
+                    alignItems={"flex-start"}
+                    spacing={0}
+                    sx={{width: '100%'}}
+                >
+                    <Typography fontSize={"14px"}>
+                        進行状況
+                    </Typography>
+                    <Typography fontSize={"18px"} fontWeight={"600"}>
+                        {props.chartSeries[0]} %
+                    </Typography>
+                </Stack>
+                <Box>
+                    <PointBar
+                        variant={"determinate"}
+                        value={props.chartSeries[0]}
+                    />
+                </Box>
+            </Stack>
+        </>
+    )
+}
