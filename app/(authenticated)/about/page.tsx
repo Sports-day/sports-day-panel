91b94d1ceb8e160cf134be902f91d0cfe58c0722
hakase61912 <hakase61912@icloud.com>
2024-10-31 16:19:41 +0900

    change : aboutページに名前を追加しました

--- app/(authenticated)/about/page.tsx
+++ app/(authenticated)/about/page.tsx
@@ -573,6 +573,63 @@ export default function Page() {
                                                              }}
                                                         >
                                                             <Typography fontSize={"16px"}
+                                                                        color={"#23398A"}>E2</Typography>
+                                                        </Box>
+                                                        <Typography variant={"h5"}>鈴木 三士郎</Typography>
+                                                    </Stack>
+                                                    <Stack
+                                                        direction={"row"}
+                                                        justifyContent={"center"}
+                                                        alignItems={"center"}
+                                                        spacing={1}
+                                                    >
+                                                        <IconButton component={Link} href={"https://github.com/suzuki346"}
+                                                                    target={"_blank"}>
+                                                            <SvgIcon>
+                                                                <FaGithubAlt color="#99a5d6"/>
+                                                            </SvgIcon>
+                                                        </IconButton>
+                                                    </Stack>
+                                                </Stack>
+                                            </CardContent>
+                                        </Card>
+                                    </motion.div>
+                                </Grid>
+
+
+                                <Grid xs={12} sm={6} lg={6}>
+                                    <motion.div
+                                        initial={{opacity: 0, y: "50px"}}
+                                        whileInView={{opacity: 1, y: "0px"}}
+                                        transition={{delay: 0.1, duration: 1, ease: [0.16, 1, 0.3, 1]}}
+                                    >
+                                        <Card sx={{height:"220px"}}>
+                                            <CardContent sx={{height:"100%"}}>
+                                                <Stack
+                                                    sx={{height:"100%"}}
+                                                    direction={"column"}
+                                                    justifyContent={"center"}
+                                                    alignItems={"center"}
+                                                    spacing={1}
+                                                    py={2}
+                                                >
+                                                    <Typography
+                                                        fontSize={"16px"}>フロントエンド開発者</Typography>
+                                                    <Stack
+                                                        direction={"row"}
+                                                        justifyContent={"center"}
+                                                        alignItems={"center"}
+                                                        spacing={1}
+                                                        py={0}
+                                                    >
+                                                        <Box py={0.4}
+                                                             px={2}
+                                                             sx={{
+                                                                 borderRadius: "15px",
+                                                                 backgroundColor: "#99a5d6"
+                                                             }}
+                                                        >
+                                                            <Typography fontSize={"16px"}
                                                                         color={"#23398A"}>E1</Typography>
                                                         </Box>
                                                         <Typography variant={"h5"}>川崎 優弥</Typography>
